fe3aefe8544dc21fca6ba754fd7bd8b5734cfbe2
Haris Amin <aminharis7@gmail.com>
2010-09-20 14:06:14 -0700

    Fixed Rails 3.0.0 compatibility issue.

--- lib/acts_as_mongo_rateable/acts_as_mongo_rateable.rb
+++ lib/acts_as_mongo_rateable/acts_as_mongo_rateable.rb
@@ -104,6 +104,5 @@ end
 %w{ models observers }.each do |dir|
   path = File.join(File.dirname(__FILE__), 'app', dir)
   $LOAD_PATH << path
-  ActiveSupport::Dependencies.load_paths << path
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
+  ActiveSupport::Dependencies.autoload_paths << path
 end
